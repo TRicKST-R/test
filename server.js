ac530b15ae2415df1698f2b28a89d54783b95c29
TRicKST-R <alexsharnin@yandex.ru>
2017-12-23 17:55:47 +0400

    response completed

--- server.js
+++ server.js
@@ -4,22 +4,57 @@ const xlsxtojson = require('xlsx-to-json-lc')
 const app = express()
 
 app.get('/sales', (req, res) => {
-    xlsxtojson(
-        {
-            input: 'db.xlsx',
-            output: null,
-            lowerCaseHeaders:true
-        },
-        (err,result) => {
-            if(err) {
-                console.log(err)
-            } 
-            res.json(result[0])
+    return new Promise(function(resolveJson, rejectJson) {
+        xlsxtojson(
+            {
+                input: 'db.xlsx',
+                output: null,
+                lowerCaseHeaders:true
+            },
+            (err,result) => {
+                if(err) {
+                    rejectJson(err)
+                } 
+                resolveJson(result)
+            }
+        )
+    })
+    .then((result) => {
+        function toInt(str){
+            return parseInt(str.replace(/-/g, ''))
         }
-    );
-    console.log(req.query.from)
-    console.log(req.query.to)
+        const dateFrom = toInt(req.query.from)
+        const dateTo = toInt(req.query.to)
+        if(dateFrom > dateTo){
+            res.status(422).json({error: 'Промежуток дат указан неправильно.'})
+        }
+        else{
+            const goods = []
+            let total_revenue = 0
+            result.map((good) => {
+                if(good['good'] !== ''){
+                    let tmpSumm = 0
+                    for(key in good){
+                        if( (toInt(key) >= dateFrom) && (toInt(key) <= dateTo) ){
+                            tmpSumm += parseFloat(good[key])
+                        }
+                    }
+                    total_revenue += tmpSumm
+                    goods.push({
+                        title: good['good'],
+                        revenue: tmpSumm.toFixed(2)
+                    })
+                }
+            })
+            res.status(200).json({
+                from: req.query.from,
+                to: req.query.to,
+                goods,
+                total_revenue: total_revenue.toFixed(2)
+            })
+        }
+    })
+    .catch((err) => console.log(err))
 })
-
 
 app.listen(3000, console.log('started on 3000'))
